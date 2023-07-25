76f0a60b50afc56403035a91b5b7dade1f94236c
VadhwanaParth15 <parthvadhwana050@gmail.com>
2023-07-25 19:56:29 +0530

    server

---
+++ App.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const https = require('https');
+const app = express();
+const port = 3000;
+const server = https.createServer(app);
+const router=require('./routes/routes');
+
+
+
+
+
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.use('/', (req, res,next) => {
+   res.status(404).send('<h1>Page not found</h1>');     
+});
+
