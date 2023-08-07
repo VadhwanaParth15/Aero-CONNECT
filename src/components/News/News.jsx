f964e1fc344731ec89181a21cddeccdb54f0639b
Parth <parthvadhwana050@gmail.com>
2023-08-07 17:01:59 +0530

    news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -15,11 +15,11 @@ const News = () => {
     <img src={rocket} alt="" />
     </div>
     <div className="section_conatnt">
-    <img src={oic} alt="" />
         <div className="content">
             <h2>Nasa's Announcement</h2>
             <p>AeroCom was founded by a group of experienced aerospace engineers who recognized the need for a centralized platform for career guidance and resources in the industry. With a shared passion for education and professional development, they set out to create a space for students and professionals to connect, learn, and grow. Today, AeroCom is a thriving digital agency dedicated to empowering the next generation of aerospace leaders.</p>
         </div>
+    <img src={oic} alt="" />
     </div>
    </section>
   )
