639772d4d7dbc3ceea839cb7f1d142885462d2b1
Parth <parthvadhwana050@gmail.com>
2023-08-09 13:27:50 +0530

    news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -20,6 +20,32 @@ const News = () => {
             <p>A team of aeronautical engineering students from [College/University Name] emerged victorious in the prestigious International Drone Design Competition. Their cutting-edge drone design showcased advancements in efficiency, control, and safety, highlighting the remarkable skills of future aeronautical engineers.</p>
         </div>
     </div>
+    <div className="section_conatnt">
+        <div className="content">
+            <h2>New Partnership Between Industry Giants and Aeronautical Department Boosts Research Opportunities</h2>
+            <p>A strategic collaboration between [Aeronautical Department] and leading aerospace companies promises to provide a wealth of research opportunities for both students and faculties. This partnership aims to accelerate innovations in aircraft design, propulsion systems, and aviation technology, strengthening the educational experience for aspiring aeronautical professionals.</p>
+        </div>
+    
+    </div>
+    <div className="section_conatnt">
+        <div className="content">
+            <h2>Aeronautics Symposium to Explore Next-Gen Materials and Manufacturing Techniques</h2>
+            <p>An upcoming symposium hosted by [University/Institute Name] will delve into the latest advancements in aeronautical materials and manufacturing processes. Faculty members and students alike will have the chance to engage with experts, learn about cutting-edge techniques, and gain insights into how these innovations are shaping the future of aviation.</p>
+        </div>
+    </div>
+    <div className="section_conatnt">
+        <div className="content">
+            <h2>Aerospace Hackathon Inspires Ingenious Solutions to Industry Challenges</h2>
+            <p>A weekend-long aerospace hackathon hosted by [Aeronautical Society/University] brought together students, professionals, and faculty members to brainstorm and develop innovative solutions to pressing challenges in the aerospace sector. The event showcased the power of collaboration and creativity among aeronautical enthusiasts.</p>
+        </div>
+    
+    </div>
+    <div className="section_conatnt">
+        <div className="content">
+            <h2>Renowned Aeronautical Professor Awarded for Outstanding Contributions to Aerospace Education</h2>
+            <p>Prof., a distinguished figure in the aeronautical field, has been honored with a prestigious award for their exceptional contributions to aerospace education. Their dedication to mentoring students and groundbreaking research has left an indelible mark on the aeronautical community.</p>
+        </div>
+    </div>
    </section>
   )
 }
